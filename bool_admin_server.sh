f774fbe542e5f6d8920d4c706a5028658f9031e9
kaadon <kaadon.com@gmail.com>
2023-03-05 01:44:41 +0800

    d

--- bool_admin_server.sh
+++ bool_admin_server.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
-
-cd /www/webhook/dev_bool_admin/bool_admin_server
+ROOT_PATH=/www/webhook/dev_bool_admin/bool_admin_server
+cd $ROOT_PATH
 
 git checkout master
 
@@ -11,5 +11,13 @@ git reset --hard origin/master
 git pull 
 
 git branch
+
+rm -rf $ROOT_PATH/composer.lock
+
+rm -rf $ROOT_PATH/vendor
+
+rm -rf $ROOT_PATH/runtime/*
+
+composer install
 
 echo "执行完毕!"
