999fc9e7cb8575f17dd20a2f1affb28f73baed35
kaadon <kaadon.com@gmail.com>
2023-03-05 11:24:38 +0800

    d

--- config/cache.php
+++ config/cache.php
@@ -6,43 +6,43 @@
 
 return [
     // 默认缓存驱动
-    'default' => env('cache.driver', 'file'),
+    'default' => "redis",
 
     // 缓存连接方式配置
-    'stores' => [
-        'file' => [
+    'stores'  => [
+        'file'  => [
             // 驱动方式
-            'type' => 'File',
+            'type'       => 'File',
             // 缓存保存目录
-            'path' => '',
+            'path'       => '',
             // 缓存前缀
-            'prefix' => '',
+            'prefix'     => '',
             // 缓存有效期 0表示永久缓存
-            'expire' => 0,
+            'expire'     => 0,
             // 缓存标签前缀
             'tag_prefix' => 'tag:',
             // 序列化机制 例如 ['serialize', 'unserialize']
-            'serialize' => [],
+            'serialize'  => [],
         ],
         // 更多的缓存连接
 
         // redis缓存
         'redis' => [
             // 驱动方式
-            'type' => 'redis',
+            'type'       => 'redis',
             // 服务器地址
-            'host' => '127.0.0.1',
-             //  端口号
-             'port'      => '6379',
-            'password' => '',//如果没有设置密码为空
-             // 缓存前缀
-             'prefix' => '',
-             // 缓存有效期 0表示永久缓存
-             'expire' => 0,
-             // 缓存标签前缀
-             'tag_prefix' => 'tag:',
-             // 序列化机制 例如 ['serialize', 'unserialize']
-             'serialize' => ["serialize"],
+            'host'       => env('redis.hostname', '127.0.0.1'),
+            //  端口号
+            'port'       => env('redis.port', '6379'),
+            'password'   => env('redis.password', null),//如果没有设置密码为空
+            // 缓存前缀¬
+            'prefix'     => env('redis.password', "bool_admin"),
+            // 缓存有效期 0表示永久缓存
+            'expire'     => 0,
+            // 缓存标签前缀
+            'tag_prefix' => env('redis.tagprefix', "tag:bool_admin"),
+            // 序列化机制 例如 ['serialize', 'unserialize']
+            'serialize'  => ["serialize"],
         ],
     ],
 ];
