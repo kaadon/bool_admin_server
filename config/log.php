cde75999380ba0b9eeba76dbe00292463b3bcc1f
kaadon <kaadon.com@gmail.com>
2023-03-05 11:38:55 +0800

    d

--- config/log.php
+++ config/log.php
@@ -20,7 +20,6 @@ return [
     'close' => false,
     // 全局日志处理 支持闭包
     'processor' => null,
-
     // 日志通道列表
     'channels' => [
         'file' => [
@@ -28,6 +27,30 @@ return [
             'type' => 'File',
             // 日志保存目录
             'path' => '',
+            // 单文件日志写入
+            'single' => true,
+            // 独立日志级别
+            'apart_level' => [],
+            // 最大日志文件数量
+            'max_files' => 500,
+            // 使用JSON格式记录
+            'json' => true,
+            // 日志处理
+            'processor' => null,
+            // 关闭通道日志写入
+            'close' => false,
+            // 日志输出格式化
+            'format' => '[%s][%s] %s',
+            // 是否实时写入
+            'realtime_write' => true,
+            'record_trace' => true,
+        ],
+
+        'test_log' => [
+            // 日志记录方式
+            'type' => 'File',
+            // 日志保存目录
+            'path' => runtime_path()."test_log",
             // 单文件日志写入
             'single' => true,
             // 独立日志级别
